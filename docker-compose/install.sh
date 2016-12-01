88129371c7fbd2219c995a032678d5f5b8827fc6
Richard Říman <riman.richard@gmail.com>
2016-12-01 17:24:46 +0100

    feat: docker-compose installer

---
+++ docker-compose/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo -n "Checking docker-compose installed... "
+TARGET=/usr/local/bin/docker-compose
+if [[ ! -f $TARGET ]]; then
+  echo "nope"
+  echo "Installing latest docker-compose..."
+  curl -k -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o $TARGET
+
+  chmod +x $TARGET
+  echo "Done."
+fi
+
+echo -n "Checking docker-compose.yml... "
+TARGET=/root/docker-compose.yml
+if [[ ! -f $TARGET ]]; then
+  echo "nope"
+  echo "Installing docker-compose.yml..."
+  curl -k -L "https://github.com/projektove/files/master/docker-compose/docker-compose.yml" -o $TARGET
+  echo "Done."
+fi
+
+echo "All done!"
